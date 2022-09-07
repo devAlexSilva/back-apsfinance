28d1e948b91e97430a1395cca294c1ad3989e616
Alex Silva <9apsilva@gmail.com>
2022-09-07 17:54:47 -0300

    feat: add authentication

---
+++ src/Authentication/auth.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import { secret } from "../Modules/login/model/SignPrismaRepository";
+
+export const Authentication = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ error: "token is required" });
+
+  const isValidToken = token.replace("Bearer ", "");
+  console.log(secret);
+
+  jwt.verify(isValidToken, secret, (err, decoded) => {
+    if (err) return res.status(401).json({ error: "invalid token" });
+
+    req.params.id = decoded?.id
+    next();
+  });
+};
