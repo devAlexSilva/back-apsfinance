689077e3a9b0136b78c42978aef660c892b8d010
Alex Silva <9apsilva@gmail.com>
2022-09-07 10:19:33 -0300

    feat: added user routes

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -2,14 +2,17 @@ import { Router } from "express";
 import { DepositeCreate } from "../Modules/deposites/controller/Create";
 import { DepositeGetAll } from "../Modules/deposites/controller/getAll";
 import { DepositeGetByName } from "../Modules/deposites/controller/getByName";
+import { CreateUserController } from "../Modules/users/controller/create";
 
 const route = Router();
-const create = new DepositeCreate().create
-const getAllDeposites = new DepositeGetAll().getAll
-const getByNameDeposites = new DepositeGetByName().get
+const create = new DepositeCreate().create;
+const getAllDeposites = new DepositeGetAll().getAll;
+const getByNameDeposites = new DepositeGetByName().get;
+const createUser = new CreateUserController().create;
 
-route.post('/deposites', create)
-route.get('/deposites', getAllDeposites)
-route.get('/deposites/:name', getByNameDeposites)
+route.post("/deposites", create);
+route.get("/deposites", getAllDeposites);
+route.get("/deposites/:name", getByNameDeposites);
+route.post("/users", createUser);
 
-export { route }
+export { route };
