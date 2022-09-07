ee66278ec8fa7692484414b2e8f150b9b9022f2d
Alex Silva <9apsilva@gmail.com>
2022-09-07 16:49:00 -0300

    feat: add login routes

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { DepositeCreate } from "../Modules/deposites/controller/Create";
 import { DepositeGetAll } from "../Modules/deposites/controller/getAll";
 import { DepositeGetByName } from "../Modules/deposites/controller/getByName";
+import { SignController } from "../Modules/login/controller/sign";
 import { CreateUserController } from "../Modules/users/controller/create";
 
 const route = Router();
@@ -9,10 +10,12 @@ const create = new DepositeCreate().create;
 const getAllDeposites = new DepositeGetAll().getAll;
 const getByNameDeposites = new DepositeGetByName().get;
 const createUser = new CreateUserController().create;
+const login = new SignController().execute;
 
 route.post("/users", createUser);
 route.post("/deposites", create);
 route.get("/deposites", getAllDeposites);
 route.get("/deposites/:name", getByNameDeposites);
+route.post("/login", login);
 
 export { route };
