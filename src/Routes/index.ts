e6f789e12cd5618f221a818aca765993b7c3f390
Alex Silva <9apsilva@gmail.com>
2022-09-08 18:20:38 -0300

    feat: added withdraw routes

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,25 +1,42 @@
 import { Router } from "express";
 import { Authentication } from "../Authentication/auth";
+import { SignController } from "../Modules/login/controller/sign";
+import { CreateUserController } from "../Modules/users/controller/create";
 import { DepositeCreate } from "../Modules/deposites/controller/Create";
 import { DepositeGetAll } from "../Modules/deposites/controller/getAll";
 import { DepositeGetByName } from "../Modules/deposites/controller/getByName";
-import { SignController } from "../Modules/login/controller/sign";
-import { CreateUserController } from "../Modules/users/controller/create";
-import { DeleteByIdController } from "../Modules/deposites/controller/deleteById";
+import { DeleteDepositeController } from "../Modules/deposites/controller/deleteById";
+import { WithdrawCreate } from "../Modules/withdraws/controller/Create";
+import { WithdrawGetAll } from "../Modules/withdraws/controller/getAll";
+import { WithdrawGetByName } from "../Modules/withdraws/controller/getByName";
+import { DeleteWithdrawController } from "../Modules/withdraws/controller/deleteById";
 
 const route = Router();
-const create = new DepositeCreate().create;
+
+const createDeposites = new DepositeCreate().create;
 const getAllDeposites = new DepositeGetAll().getAll;
 const getByNameDeposites = new DepositeGetByName().get;
+const deleteDeposite = new DeleteDepositeController().execute;
+
 const createUser = new CreateUserController().create;
 const login = new SignController().execute;
-const deleteDeposite = new DeleteByIdController().execute;
+
+const createWithdraw = new WithdrawCreate().create;
+const getAllWithdraw = new WithdrawGetAll().getAll;
+const getByNameWithdraw = new WithdrawGetByName().get;
+const deleteWithdraw = new DeleteWithdrawController().execute;
 
 route.post("/login", login);
 route.post("/users", createUser);
-route.post("/deposites", Authentication, create);
+
+route.post("/deposites", Authentication, createDeposites);
 route.get("/deposites", Authentication, getAllDeposites);
 route.get("/deposites/:name", Authentication, getByNameDeposites);
 route.delete("/deposites", Authentication, deleteDeposite);
+
+route.post("/withdraws", Authentication, createWithdraw);
+route.get("/withdraws", Authentication, getAllWithdraw);
+route.get("/withdraws/:name", Authentication, getByNameWithdraw);
+route.delete("/withdraws", Authentication, deleteWithdraw);
 
 export { route };
