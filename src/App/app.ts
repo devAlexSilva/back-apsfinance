612c1c9b0a15db99e2518302b2a1f98f1026481f
Alex Silva <9apsilva@gmail.com>
2022-09-14 18:57:50 -0300

    fix: alter place of the cors

--- src/App/app.ts
+++ src/App/app.ts
@@ -1,12 +1,7 @@
 import express from "express";
 import { app } from "./server";
 import { route } from "../Routes";
-import cors from 'cors'
 
 app.use(express.json());
-app.use(cors())
-
-
 app.use(route);
 app.get("/", (req, res) => res.send("APS-Finance"));
-
--- src/App/server.ts
+++ src/App/server.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 2727;
+app.use(cors());
 
 app.listen(port, () => console.log(`⚙️ http://localhost:${port} `));
 
