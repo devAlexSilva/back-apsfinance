34041ecd7229291afc5139376a137cab401fb9ad
Alex Silva <9apsilva@gmail.com>
2022-09-14 18:07:24 -0300

    chore: add config to deploy

--- src/App/server.ts
+++ src/App/server.ts
@@ -1,8 +1,10 @@
-import express from "express"
+import dotenv from "dotenv";
+import express from "express";
 
-const app = express()
-const port = process.env.PORT || 2727
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 2727;
 
-app.listen(port, () => console.log(`⚙️ http://localhost:${port} `))
+app.listen(port, () => console.log(`⚙️ http://localhost:${port} `));
 
-export { app }
+export { app };
